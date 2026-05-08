d070a8c36ad1a22a6fc1c1f582fc5e4df4de4ad0
lsngmin <lsngminn@gmail.com>
2026-05-08 12:53:30 +0900

    chore: scaffold Spring Boot Kotlin project

---
+++ src/main/kotlin/com/yourname/bot/PrReviewBotApplication.kt
@@ -0,0 +1,11 @@
+package com.yourname.bot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrReviewBotApplication
+
+fun main(args: Array<String>) {
+	runApplication<PrReviewBotApplication>(*args)
+}
---
+++ src/test/kotlin/com/yourname/bot/PrReviewBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yourname.bot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrReviewBotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
